232ed7189ab0b06e6ec9d0320f28d240671237a7
Naderschah <felix-semler@web.de>
2023-07-03 11:31:57 +0200

    Scripts work now

--- Data_Collection_Processing/cron_scripts/auto_start_script.py
+++ Data_Collection_Processing/cron_scripts/auto_start_script.py
@@ -5,6 +5,7 @@ All methods I found for automatically launching applications on a raspberry are 
 So this is supposed to be a script run by cron to check if a script is running and then start it 
 """
 script_name = 'allsky-v7.0.py'
+
 import psutil
 import sys
 from subprocess import Popen
@@ -12,9 +13,12 @@ import datetime
 import os
 
 for process in psutil.process_iter():
-    if process.cmdline() == ['python', script_name]:
+    cmd = process.cmdline()
+    # Check if keywords present
+    if  any(['python' in i for i in cmd]) and any([script_name in i for i in cmd]):
          sys.exit('Process found: exiting.')
 # Make note when script was noticed to be off (in case we want to make running diagnostics)
 with open(os.path.join(os.environ['HOME'], 'script_off.txt'), 'a') as f:
     f.write(datetime.datetime.now().strftime('%Y%m%d-%H%M%S'))
-Popen(['python', 'update.py'])
+# Execute new child thread
+Popen(['/usr/bin/python',os.path.join(os.environ['HOME'],script_name), '>>', os.path.join(os.environ['HOME'], 'Imaging_Output.txt')])
--- Data_Collection_Processing/cron_scripts/camera_dead.py
+++ Data_Collection_Processing/cron_scripts/camera_dead.py
@@ -8,20 +8,38 @@ import subprocess
 import psutil
 import datetime
 import sys
+import time
+import os
+import telegram
+
+# Tele bot token and chat_id (to be placed in .bashrc)
+token = os.environ['TOKEN']
+chat_id = os.environ['CHAT_ID']
 
 for process in psutil.process_iter():
     counter = 0
     while True:
-        if 'gphoto' in process.cmdline():
+        if any(['gphoto' in i for i in process.cmdline()]):
             # Wait 30 sec for camera to finish
             time.sleep(30)
             counter+=1
         else:
             out = len(subprocess.check_output('gphoto2 --auto-detect').split(b'\n'))
             if out < 4:
-                with open(os.path.join('/tmp', 'CAMERA_GONE')):
+                # Make file that can be read from other machines
+                with open(os.path.join('/tmp', 'CAMERA_GONE')) as f:
                     f.write(' ')
-                with open(os.path.join(so.environ['HOME'], 'CAMERA_DEAD.log')):
+                # Log time in case we ever do debugging
+                with open(os.path.join(os.environ['HOME'], 'CAMERA_DEAD.log')):
                     f.write(datetime.now().strftime('%Y%m%d-%H%M%S'))
+                # Send message over telegram bot
+                bot = telegram.Bot(token)
+                async with bot:
+                    try: # In case hostname is not specific enough this environemnt variable can be set (in bashrc remember they are not persistent)
+                        name = os.environ['DESCRIPTOR']
+                    except:
+                        name = os.environ['HOSTNAME']
+                    await bot.send_message(text='Camera of Device {} is down'.format(name), chat_id=chat_id)
+        # Timer in case it gets stuck somewhere
         if counter == 3:
             sys.exit(0)
