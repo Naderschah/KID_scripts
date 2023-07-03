ec65a1ac1b7869276cd56280703e6617a7beded0
Naderschah <felix-semler@web.de>
2023-07-03 17:21:15 +0200

    Cant wait for board to recognize changes before camera started

--- Data_Collection_Processing/data_collection.py
+++ Data_Collection_Processing/data_collection.py
@@ -559,7 +559,7 @@ class Camera_Handler_picamera:
         # Configure sensor mode etc
         self.camera.configure(self.capture_config)
         # Set other settings
-        self.set_controls()
+        self.camera.set_controls(self.ctrl)
         return
 
 
