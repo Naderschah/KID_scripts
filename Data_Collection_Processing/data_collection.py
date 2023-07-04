664dfb48691be609666669f74769ebc25909e99e
Naderschah <felix-semler@web.de>
2023-07-04 10:01:29 +0200

    another in in

--- Data_Collection_Processing/data_collection.py
+++ Data_Collection_Processing/data_collection.py
@@ -752,7 +752,7 @@ class Camera_Handler_picamera:
             for j in gain_range:
                 self.ctrl['AnalogueGain'] = j
                 self.camera.set_controls(self.ctrl)
-                for k in in num_im:
+                for k in num_im:
                     self.capture_image_and_download(name='{}_{}.dng'.format(i,j))
         return
 
