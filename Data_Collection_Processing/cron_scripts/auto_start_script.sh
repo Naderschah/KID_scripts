6ddfe5832ef7068530f1aad9406d9bf89d579d49
Naderschah <felix-semler@web.de>
2023-07-07 14:36:37 +0200

    Piping doesnt create files so added explicit file creation

--- Data_Collection_Processing/cron_scripts/auto_start_script.sh
+++ Data_Collection_Processing/cron_scripts/auto_start_script.sh
@@ -7,14 +7,17 @@ HOME=/home/EOSRP/
 # Make sure to have this executable
 script_name=allsky-v7.0.py
 
-
 process_counts=$(ps aux | grep -i 'python' | grep -i $script_name | wc -l)
 
 # 0 is true 1 is false
 if [ $process_counts -eq 0 ]
 then
+    # Piping doesnt create files
+    if [ ! -e ${HOME}Imaging_Output.txt ] ; then
+        touch ${HOME}Imaging_Output.txt
+    fi
     echo "Start Script"
-    /usr/bin/python $HOME/$script_name >>  $HOME/Imaging_Output.txt
+    /usr/bin/python $HOME$script_name >>  ${HOME}Imaging_Output.txt
 else
     echo "Script running"
 fi
